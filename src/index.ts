d85ef04eec82061abb726abaeeae546dd8b8cf11
Jui <cse_1912020087@gmail.com>
2024-05-03 21:26:46 +0600

    primitive

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+// Basic Data type
+
+
+// Primitive DataType:
+
+// string
+let firstName:string ="Jui"
+// number
+let roll:number=123;
+// boolean
+let isAdmin = true;
+// undefined
+let x: undefined =undefined;
+// null
+let y:null =null;
+// Any  
+let d;
+d='Jui'
+d=123;
+d=null;
+d=undefined;
+//Any type using type
+let a:number
+a=345;
+
+
+// Non-Primitive dataType:
+
+//Array
+let friends:string[] = ['rachel','monica'];
+let rollList :number[] = [1,2]
+
+// Tuple -> special type of Array -> order -> type of value
+
+let profileCoordinate :[string,number,boolean] =['Jui',1234,true];
+
+let numbers :[number,number];
+numbers =[1,2];
+
+
+
+
