eaeabcc6cb832bea396afcd52dc5299c3994c61b
Jui <cse_1912020087@gmail.com>
2024-05-03 21:52:08 +0600

    non-primitive

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log('Hello');
+// Basic Data type
+// Primitive DataType:
+// string
+let firstName = "Jui";
+// number
+let roll = 123;
+// boolean
+let isAdmin = true;
+// undefined
+let x = undefined;
+// null
+let y = null;
+// Any  
+let d;
+d = 'Jui';
+d = 123;
+d = null;
+d = undefined;
+//Any type using type
+let a;
+a = 345;
+// Non-Primitive dataType or Reference Type:
+//Array
+let friends = ['rachel', 'monica'];
+let rollList = [1, 2];
+// Tuple -> special type of Array -> order -> type of value
+let profileCoordinate = ['Jui', 1234, true];
+let numbers;
+numbers = [1, 2];
+// Object
+const user = {
+    firstName: 'Sumaiya',
+    lastName: 'Jui',
+    company: "Programming Hero",
+    isMarried: true,
+};
